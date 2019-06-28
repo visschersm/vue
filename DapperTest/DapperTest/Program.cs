9a234214b6aa8143007db3b03d8c9bca37013aff
Mark Visschers <Mark.Visschers@teamrockstars.nl>
2019-06-28 14:11:43 +0200

    gitignore fixed?

---
+++ DapperTest/DapperTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DapperTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sqlOrderDetails = "SELECT TOP 5 * FROM OrderDetails;";
+            string sqlOrderDetail = "SELECT * FROM OrderDetails WHERE OrderDetailID = @OrderDetailID;";
+            string sqlCustomerInsert = "INSERT INTO Customers (CustomerName) Values (@CustomerName);";
+
+            using (var connection = new SqlConnection(FiddleHelper.GetConnectionStringSqlServerW3Schools()))
+            {
+                var orderDetails = connection.Query<OrderDetail>(sqlOrderDetails).ToList();
+                var orderDetail = connection.QueryFirstOrDefault<OrderDetail>(sqlOrderDetail, new { OrderDetailID = 1 });
+                var affectedRows = connection.Execute(sqlCustomerInsert, new { CustomerName = "Mark" });
+
+                Console.WriteLine(orderDetails.Count);
+                Console.WriteLine(affectedRows);
+
+                FiddleHelper.WriteTable(orderDetails);
+                FiddleHelper.WriteTable(new List<OrderDetail>() { orderDetail });
+            }
+        }
+    }
+}
