63a9fccf488443c26af74b873547f592d702a644
Mark Visschers <mark.visschers@teamrockstars.nl>
2020-03-16 14:10:40 +0100

    vue tutorials

---
+++ vue/getting_started/app04_list/index.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app-4',
+    data: {
+      todoItems: [
+        { title: 'Learn JavaScript' },
+        { title: 'Learn TypeScript' },
+        { title: 'Learn Vue' },
+        { title: 'Learn Angular' },
+        { title: 'Learn Blazor' }
+      ]
+    }
+  });
---
+++ vue/project02/index.js
@@ -0,0 +1,5 @@
+var data = {a: 1}
+
+var vm = new Vue({
+    data: data
+});
